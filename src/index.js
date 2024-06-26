d2c407de2fd1bc9fa92ba1ab1bd35ff45f661b94
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-06-26 20:05:00 +0530

    path changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './app';
+import Home from './views/Home/home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
